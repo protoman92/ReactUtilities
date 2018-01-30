b9464dd430b8d719801fd15c7e54b692fbf4229f
Hai Pham <viethai.pham@holmusk.com>
2018-01-31 02:45:45 +0800

    Navigator now requires view model for navigate(), not just screen

--- src/mvvm/Navigator.ts
+++ src/mvvm/Navigator.ts
@@ -1,4 +1,5 @@
 import * as Navigation from './navigation';
+import * as ViewModel from './viewmodel';
 
 /**
  * We should have a dedicated navigator that view models have access to, in
@@ -8,10 +9,10 @@ import * as Navigation from './navigation';
 export interface Type {
   /**
    * Navigate to a screen that owns a particular view model.
-   * @param {Navigation.Screen.Type} screen The screen to navigate to.
+   * @param {ViewModel.Type} vm A ViewModel instance.
    * @param {Navigation.Info.Type} intent The navigation information.
    */
-  navigate(screen: Navigation.Screen.Type, info: Navigation.Info.Type): void;
+  navigate(vm: ViewModel.Type, info: Navigation.Info.Type): void;
 
   /**
    * Navigate back to the previous screen.
