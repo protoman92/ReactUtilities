68a19fcea514a9916dd26da4a5c1b4d23ec770b7
Hai Pham <swiften.svc@gmail.com>
2018-08-25 15:46:38 +0800

    fix(export): fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { LifecycleHooks, LifecycleHooksHOCOptions, withLifecycleHooks } from 'hoc.lifecycle';
 export { CompleteSetupHOCOptions, withCompleteSetup } from './hoc.all';
+export { LifecycleHooks, LifecycleHooksHOCOptions, withLifecycleHooks } from './hoc.lifecycle';
+export { LoadableHOCOptions, LoadableHooks, withLoadable, withTestLoadable } from './hoc.loadable';
 export { ViewModelFactoryHOCProps, ViewModelHOCOptions, ViewModelHOCProps, ViewModelHooks, withViewModel } from './hoc.viewmodel';
 export { ReduxType as ReduxViewModel, RootType as RootViewModel } from './viewmodel';
